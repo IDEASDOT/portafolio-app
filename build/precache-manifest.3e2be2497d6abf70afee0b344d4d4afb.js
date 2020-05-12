0b6a4f39afd8d4f89e0e0b18b9724373238bf917
IDEASDOT <ideasdot@gmail.com>
2020-05-12 03:49:38 -0700

    first commit

---
+++ build/precache-manifest.3e2be2497d6abf70afee0b344d4d4afb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd67d149a5c9d7348020665a0e37c01c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05c166c6e59885dfbb2c",
+    "url": "/static/css/main.59489703.chunk.css"
+  },
+  {
+    "revision": "67a445ae9254f98669ed",
+    "url": "/static/js/2.03b31d6a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.03b31d6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05c166c6e59885dfbb2c",
+    "url": "/static/js/main.dcb78dc8.chunk.js"
+  },
+  {
+    "revision": "ef57fcd4a0589b04ed9d",
+    "url": "/static/js/runtime-main.0483e99c.js"
+  }
+]);
